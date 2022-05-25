1fa5906817e4d7bc6e565d4a6d2c44f2cbb85241
Vinicius Navarro <vinicius.navarro@matera.com>
2022-05-25 18:18:45 -0300

    Initial commit

---
+++ src/test/java/br/com/aluraapi/forum/ForumApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.aluraapi.forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
